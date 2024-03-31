4dfb453e12af66c1a28acabbc2e3fd6e992ce41a
shinshka <151514026+shinshka@users.noreply.github.com>
2024-03-31 20:47:29 +0600

    Add files via upload
    govno govna

--- loader.py
+++ loader.py
@@ -3,6 +3,6 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.redis import RedisStorage2
 
-bot = Bot(token='1731506104:AAFTSw03X1CIQf8zv4TJ-AHto7a0gAFRjrU', parse_mode=types.ParseMode.HTML)
-storage = RedisStorage2(port=7777)
+bot = Bot(token='6803792823:AAHcezYfCtEFtB05-t_nJGvZKTn3w1V8LDk', parse_mode=types.ParseMode.HTML)
+storage = RedisStorage2(port=6379)
 dp = Dispatcher(bot, storage=storage)
--- utils/sender.py
+++ utils/sender.py
@@ -2,7 +2,7 @@ import sys
 
 import requests
 
-token = '1731506104:AAFTSw03X1CIQf8zv4TJ-AHto7a0gAFRjrU'
+token = '6803792823:AAHcezYfCtEFtB05-t_nJGvZKTn3w1V8LDk'
 
 request = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={sys.argv[1]}&text={sys.argv[2]}"
 requests.get(request)
